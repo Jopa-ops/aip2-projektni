03e412bde02b27ea425901d96bd7475026d37115
Jopa-ops <lovasicjosip@gmail.com>
2025-06-05 06:30:25 +0000

    promjena zadnja

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <cctype>
 using namespace std;
 
 int main()
@@ -7,8 +8,10 @@ int main()
     string rijec;
     cout << "Unesi riječ koju treba pogoditi: ";
     cin >> rijec;
+
     string skrivena(rijec.length(), '_');
     int pokusaji = 6;
+
     while (skrivena != rijec && pokusaji > 0)
     {
         cout << "\nRiječ: " << skrivena << "\n";
@@ -16,29 +19,38 @@ int main()
         cout << "Unesite slovo: ";
         char slovo;
         cin >> slovo;
+
+        slovo = tolower(slovo); // pretvori unos u malo slovo
         bool pogodak = false;
+
         for (int i = 0; i < rijec.length(); i++)
         {
-            if (rijec[i] == slovo)
+            if (tolower(rijec[i]) == slovo && skrivena[i] == '_') // uspoređuj neovisno o veličini slova
             {
-                skrivena[i] = slovo;
+                skrivena[i] = rijec[i]; // zadrži originalno slovo (veliko ili malo)
                 pogodak = true;
             }
-        } 
-        if(!pogodak)
+        }
+
+        if (!pogodak)
         {
-            cout << "Promašaj!";
-            pokušaji--;
-        } else {
-            cout<<"Pogodak!\n"
+            cout << "Promašaj!\n";
+            pokusaji--;
+        }
+        else
+        {
+            cout << "Pogodak!\n";
         }
     }
-    if(skrivena == rijec)
+
+    if (skrivena == rijec)
     {
-        cout << "\n Čestitamo! Pogodio si riječ." << rijec << endl;
+        cout << "\nČestitamo! Pogodio si riječ: " << rijec << endl;
     }
     else
     {
-        cout << "Izgubio si. Tražena riječ je bila:" << rijec << endl;
-        return 0;
+        cout << "Izgubio si. Tražena riječ je bila: " << rijec << endl;
     }
+
+    return 0;
+}
